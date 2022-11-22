0dd621ca74390bf5304d38fcfba4df4f0c9382f2
nvshink <nikinbox16@gmail.com>
2022-11-22 17:24:23 +0600

    Печать готова к публикации

--- src/http-common.js
+++ src/http-common.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  // baseURL: "https://v-put.herokuapp.com/api",
-  baseURL: "http://localhost:8080/api",
+  baseURL: "https://v-put.herokuapp.com/api",
   headers: {
     "Content-type": "application/json"
   }
