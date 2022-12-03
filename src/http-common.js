8400784d74e7022c340df46ed395413c1f257574
nvshink <nikinbox16@gmail.com>
2022-12-03 11:38:54 +0600

    Получилось?

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "web-production-1648.up.railway.app/api",
+  baseURL: "http://web-production-1648.up.railway.app/api",
   //baseURL: "http://localhost:8080/api",
   headers: {
     "Content-type": "application/json"
