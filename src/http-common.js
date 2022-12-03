23ef25653659fcb6a726d8bb89a2033c062c4c55
nvshink <nikinbox16@gmail.com>
2022-12-03 11:26:40 +0600

    Итого

--- src/http-common.js
+++ src/http-common.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-  //baseURL: "https://v-put.herokuapp.com/api",
-  baseURL: "http://localhost:8080/api",
+  baseURL: "web-production-1648.up.railway.app/api",
+  //baseURL: "http://localhost:8080/api",
   headers: {
     "Content-type": "application/json"
   }
